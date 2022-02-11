6e7cd46c5c941af604f1064f406a9f40d18e6f89
fred <310bel@gmail.com>
2022-02-11 16:27:35 +0300

    12 урок

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,5 @@ $groupData = [
 Route::group($groupData, function () {
     // BlogCategory
     $methods = ['index', 'edit', 'store', 'update', 'create'];
-    Route::resource('categories', 'CategoryController')->only($methods) ->name('blog.admin.categories');
+    Route::resource('categories', 'CategoryController')->only($methods) ->names('blog.admin.categories');
 });
