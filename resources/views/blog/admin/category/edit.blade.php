304f665eda077615f3874461d915b85516c3f622
fred <310bel@gmail.com>
2022-02-15 03:30:16 +0300

    17 урок

--- resources/views/blog/admin/category/edit.blade.php
+++ resources/views/blog/admin/category/edit.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
     @php /** @var \App\Models\BlogCategory $item */ @endphp
-<form method="POST" action="{{ route('blog.admin.categories.update', $item->id) }}">
-    @method('PATCH')
-    @csrf
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                @include('blog.admin.category.includes.item_edit_main_col')
-            </div>
-            <div class="col-md-3">
-                @include('blog.admin.category.includes.item_edit_add_col')
+    <form method="POST" action="{{ route('blog.admin.categories.update', $item->id) }}">
+        @method('PATCH')
+        @csrf
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    @include('blog.admin.category.includes.item_edit_main_col')
+                </div>
+                <div class="col-md-3">
+                    @include('blog.admin.category.includes.item_edit_add_col')
+                </div>
             </div>
         </div>
-        </div>
-</form>
+    </form>
 @endsection
